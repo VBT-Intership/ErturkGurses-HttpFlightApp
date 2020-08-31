64586130d489ae89d09ad3c697fc76bf4ec0c88f
erturkgurses <erturkgurses@hotmail.com>
2020-08-31 23:54:33 +0300

    Changed HomePage to statefulWidget

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:ticket_app/components/flight_card.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key key}) : super(key: key);
 
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ListView.builder(
-          padding: EdgeInsets.only(right: 20, left: 20),
-          itemExtent: MediaQuery.of(context).size.height / 5,
-          itemCount: 5,
-          itemBuilder: (context, index) {
-            return FlightCard();
-          }),
-    );
+    return ListView.builder(
+        padding: EdgeInsets.only(right: 20, left: 20),
+        itemExtent: MediaQuery.of(context).size.height / 5,
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return FlightCard();
+        });
   }
 }

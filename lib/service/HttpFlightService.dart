e34cf2a3b1856122ff255eada94b290c16523f7e
erturkgurses <erturkgurses@hotmail.com>
2020-08-31 22:50:54 +0300

    Created flight service

---
+++ lib/service/HttpFlightService.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+import 'dart:html';
+
+import 'package:ticket_app/configs/constants.dart';
+import 'package:ticket_app/model/flight_model.dart';
+import 'package:ticket_app/service/IHttpFlightService.dart';
+import 'package:http/http.dart' as http;
+
+abstract class IHttpFlightService {
+  Future<List<FlightModel>> getFlightList();
+}
+
+class HttpFlightService extends IHttpFlightService {
+  @override
+  Future<List<FlightModel>> getFlightList() async {
+    var url = Constants.baseUrl + "Flights";
+    var response = await http.get(url);
+    List<FlightModel> flightList = List();
+
+    switch (response.statusCode) {
+      case HttpStatus.ok:
+        final jsonBody = jsonDecode(response.body);
+        if (jsonBody is List)
+          flightList = jsonBody.map((e) => FlightModel.fromJson(e)).toList();
+        break;
+      default:
+    }
+    return flightList;
+  }
+}

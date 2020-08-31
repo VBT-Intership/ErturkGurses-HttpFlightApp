9a93bbb1bc1deb80bc7379bac7111587e50a60fe
erturkgurses <erturkgurses@hotmail.com>
2020-08-31 22:45:53 +0300

    Added base url

--- lib/configs/constants.dart
+++ lib/configs/constants.dart
@@ -3,5 +3,6 @@ import 'package:flutter/material.dart';
 class Constants {
   static const firstColor = Color.fromRGBO(31, 74, 168, 1);
   static const secondColor = Color.fromRGBO(217, 96, 85, 1);
+  static const baseUrl = "https://flighttickets-bcbf1.firebaseio.com/";
 
 }

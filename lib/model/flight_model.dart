1e27c29394018f1022b88eb19d7da111e7524ae3
erturkgurses <erturkgurses@hotmail.com>
2020-09-01 01:11:11 +0300

    Changed flight model

--- lib/model/flight_model.dart
+++ lib/model/flight_model.dart
@@ -1,36 +1,18 @@
 class FlightModel {
-  AKN21 aKN21;
-
-  FlightModel({this.aKN21});
-
-  FlightModel.fromJson(Map<String, dynamic> json) {
-    aKN21 = json['AKN21'] != null ? new AKN21.fromJson(json['AKN21']) : null;
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    if (this.aKN21 != null) {
-      data['AKN21'] = this.aKN21.toJson();
-    }
-    return data;
-  }
-}
-
-class AKN21 {
   String date;
   int flightDuration;
   String flightTime;
   String fromWhere;
   String toWhere;
 
-  AKN21(
+  FlightModel(
       {this.date,
       this.flightDuration,
       this.flightTime,
       this.fromWhere,
       this.toWhere});
 
-  AKN21.fromJson(Map<String, dynamic> json) {
+  FlightModel.fromJson(Map<String, dynamic> json) {
     date = json['date'];
     flightDuration = json['flightDuration'];
     flightTime = json['flightTime'];

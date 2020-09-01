5c1c3d575cacfe383c4af223adbcbe9348453049
erturkgurses <erturkgurses@hotmail.com>
2020-09-02 00:18:17 +0300

    Added delete method

--- lib/service/IHttpFlightService.dart
+++ lib/service/IHttpFlightService.dart
@@ -1,8 +1,6 @@
 import 'package:ticket_app/model/flight_model.dart';
 
-
 abstract class IHttpFlightService {
-
   Future<List<FlightModel>> getFlightList();
-
+  Future<void> deleteFlight(int index);
 }
